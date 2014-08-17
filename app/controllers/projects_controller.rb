719234336673701daead4b45c8b8266425b4a038
ura <byorickq@gmail.com>
2014-08-17 17:05:49 +0300

    Ajax CRUD for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,39 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+    respond_to :js
+  end
+
+  def create
+    @project = Project.new project_params
+    if @project.save 
+      respond_to :js 
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      respond_to :js 
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    respond_to :js
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:name)
+    end
 end
