7b62f57c967a7eb7d8b2429e7d670308f33e340c
ura <byorickq@gmail.com>
2014-08-23 12:28:55 +0300

    added sortable feature frontend

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('.checkmark').click(function(event) {
     $('this').closest('input').prop('checked', true);
   });
+  $('.tasks-list').sortable({
+      placeholder: "ui-state-highlight",
+      axis: "y"
+    });
 });
